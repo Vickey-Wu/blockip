e08ec0cfa496122738d082c4a095c5ca20c7f489
Vickey-Wu <vickeywu557@gmail.com>
2020-02-26 15:37:52 +0800

    fix: change nginx log name

--- settings.py
+++ settings.py
@@ -15,7 +15,7 @@ FREQUENCY = 3
 
 # the api route that need to block ip access, NOT NULL
 ROUTE = '/api/testblock'
-NGINX_LOG_PATH = '/var/log/nginx/tb_access.log'
+NGINX_LOG_PATH = '/var/log/nginx/your_nginx.log'
 
 # the first time the ip over limit default block 600s (10mins)
 BLOCK_TIME = 600
